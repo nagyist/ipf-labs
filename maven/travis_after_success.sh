0336b02c88236bc24fd8191f8fea0a4a33c19584
Christian Ohr <christian.ohr@gmail.com>
2018-10-18 17:31:22 +0200

    less noise during upload

--- maven/travis_after_success.sh
+++ maven/travis_after_success.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 wget -q https://raw.githubusercontent.com/oehf/ipf-labs/master/maven/settings.xml --no-check-certificate
-mvn clean deploy -DskipTests=true --settings settings.xml
+mvn -B clean deploy -DskipTests=true --settings settings.xml
